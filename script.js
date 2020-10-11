14f35324a2bdce8d932810ffaa80c0bae6afab69
RajagopalVenkataramani <vrajagopalaswin@gmail.com>
2020-10-11 22:42:25 +0530

    index updated and javascript first class

---
+++ script.js
@@ -0,0 +1,25 @@
+// client side scripting language
+// exists on its own
+// executed by the inbuilt compiler of the browser
+alert("Hai, Welcome to Javascript Programming");
+var vlue;
+
+
+//  Date datatype
+
+var today = new Date();
+var d1 = new Date(1000); 
+var d2 = new Date("03/04/2020");
+var d3 = new Date(1994,10,15);
+var d4 = new Date(1994,10);
+var d5 = new Date(1994,10,15,16);
+var d6 = new Date(1994,10,15,16,15);
+var d7 = new Date(1994,10,15,16,15,32);
+var d8 = new Date(1994,10,15,16,15,32,58);
+var d9 = new Date(1994,10,15,16,15,32,26,34);
+var d9 = new Date(1994,10,15,16,15,32,26,34);
+var d10 = new Date(2000,1,45);
+console.log(d10);
+
+
+// Array
